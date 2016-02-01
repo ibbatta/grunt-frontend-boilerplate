e1f1a10244fc3f2b956bcabef4d78f4256f2900d
Maurizio Battaghini <maurizio.battaghini@jobrapido.com>
2016-02-01 16:33:38 +0100

    define gruntfile for basical tasks create express server while start grunt and watch for modiy restructure tree

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,6 +8,73 @@ module.exports = function(grunt) {
     // get the configuration info from package.json
     pkg: grunt.file.readJSON('package.json'),
 
+    // JSHINT
+    jshint: {
+      options: {
+        reporter: require('jshint-stylish')
+      },
+      all: ['Grunfile.js', 'src/**/*.js']
+    },
+
+    // UGLIFY
+    uglify: {
+      build: {
+        files: {
+          'dist/js/main.min.js': 'app/**/*.js'
+        }
+      }
+    },
+
+    // COMPILE SASS
+    sass: {
+      options: {
+        noCache: true,
+        update: true,
+        sourcemap: 'none'
+      },
+      dist: {
+        files: {
+          'dist/css/main.css': 'app/index.scss'
+        }
+      }
+    },
+
+    // MINIFY CSS
+    cssmin: {
+      build: {
+        files: {
+          'dist/css/main.min.css': 'dist/css/main.css'
+        }
+      }
+    },
+
+    // WATCH
+    watch: {
+      options:{
+        livereload: true
+      },
+
+      stylesheet: {
+        files: ['app/index.scss'],
+        tasks: ['sass', 'cssmin']
+      },
+      scripts: {
+        files:['app/**/*.js'],
+        tasks:['jshint', 'uglify']
+      }
+    },
+
+    // EXPRESS SERVER
+    express:{
+      all:{
+        options:{
+          port:9001,
+          hostname:'localhost',
+          bases:['./app'],
+          livereload: true 
+        }
+      }
+    }
     
 
   });
@@ -18,8 +85,8 @@ module.exports = function(grunt) {
   require('load-grunt-tasks')(grunt);
 
   // ===========================================================================
-  // RUN GRUNT TASKS ========================================================
+  // RUN GRUNT TASKS ===========================================================
   // ===========================================================================
-  grunt.registerTask('serve',[]);
+  grunt.registerTask('default', ['express', 'watch']);
 
 };
